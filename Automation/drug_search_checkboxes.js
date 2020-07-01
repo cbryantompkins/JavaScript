4b08b93f101fcb71791be89ea3a2d26d1393f460
ctompkins-mmit <65771709+ctompkins-mmit@users.noreply.github.com>
2020-07-01 18:21:56 -0400

    add drug search checkboxes code

---
+++ Automation/drug_search_checkboxes.js
@@ -0,0 +1,5 @@
+// clicks check all of Formulary filters menu
+function () {document.querySelectorAll('.ui-icon-check')[index].click()
+
+// chicks Uncheck All
+function () {document.querySelectorAll('ui-icon-closethick')[index].click()
