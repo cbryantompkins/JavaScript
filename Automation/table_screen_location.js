d6a3beacd2f31cc6ceabeddf4bada1edffc26671
Curtis Tompkins <ctompkins@mmitnetwork.com>
2020-07-14 23:26:06 -0400

    new automation

---
+++ Automation/table_screen_location.js
@@ -0,0 +1,23 @@
+const tableArray_1 = []
+
+const getTable_1 = function () {
+
+  var table = document.getElementById('bv-multilineGrid');
+  var table_pos = table.getBoundingClientRect()
+  var x_pos = table_pos.x
+  var y_pos = table_pos.y
+  tableArray_1.push(x_pos, y_pos)
+
+}
+
+const tableArray_2 = []
+
+const getTable_2 = function () {
+
+  var table = document.getElementById('bv-multilineGrid');
+  var table_pos = table.getBoundingClientRect()
+  var x_pos = table_pos.x
+  var y_pos = table_pos.y
+  tableArray_2.push(x_pos, y_pos)
+
+}
