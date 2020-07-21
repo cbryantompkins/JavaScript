8b035310949214c33c85ebde81c8a797f5f2eded
ctompkins-mmit <65771709+ctompkins-mmit@users.noreply.github.com>
2020-07-21 14:29:37 -0400

    up

--- Automation/Radar Dates varibles.js
+++ Automation/Radar Dates varibles.js
@@ -1,13 +1,10 @@
-function radar(){
-//begins promise
-return new Promise((resolve,reject) => {
 //uses Date prototype to add custom function that subtracts a number of days (variable 'd')
 Date.prototype.subtractDays = function(d) {
         var date = new Date(this.valueOf());
         date.setDate(date.getDate()-d);
         return date;
 }
-//function with two paramenters, the start and stop getDates
+//function with two paramenters, the start and stop Dates
 // creates a new array and pushes the date -1 day to array until stop date in the current Date
 //creates a list of sequential dates
   function getDates(startDate, stopDate) {
@@ -22,7 +19,7 @@ Date.prototype.subtractDays = function(d) {
 //using getDates from above, takes two parameters - here, the current date minus 6 and todays Date
 // then iterates over the array created by getDates and transforms them, then splices them into the Array
 // results in an array of dates like 'Jul 14, 2020'
-var dateArray = getDates(new Date().subtractDays(6), new Date());
+var dateArray = getDates(new Date().subtractDays(7), new Date());
 for (let i = 0; i < dateArray.length; i++ ) {
   var date = dateArray[i];
   var month = date.toLocaleString('default', {month: 'short'});
@@ -32,15 +29,11 @@ for (let i = 0; i < dateArray.length; i++ ) {
   dateArray.splice(i, 1, fullDate);
 }
 //debuggng log for array
-console.log(dateArray);
-//selects all update dates on radar surveillance page
-var updateDate = document.querySelectorAll('.update-date');
-//iterates over update dates checking against date array to make sure all match
-for (var i = 0; i < updateDate.length; i++) {
-  if (new RegExp(dateArray.join("|")).test(updateDate[i].innerText) == false); {
-    reject('Unexpected Date Found');
-  	}
-  }
-  resolve();
-});
-}
+console.log(dateArray[0]);
+console.log(dateArray[7]);
+
+const thisWeek = dateArray[0]
+const lastWeek = dateArray[7]
+
+exportsTest.lastWeekDate = lastWeek;
+exportsTest.thisWeekDate = thisWeek;
