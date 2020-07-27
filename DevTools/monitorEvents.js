f0ecea7aec0ce4cccd229450829f406f04e134b8
Curtis Tompkins <ctompkins@mmitnetwork.com>
2020-07-27 16:46:39 -0400

    up

--- DevTools/monitorEvents.js
+++ DevTools/monitorEvents.js
@@ -1,1 +1,11 @@
+//all Events
+monitorEvents(window)
+
+//all events  verbose
 monitorEvents(window, ['mouse', 'key', 'touch', 'control'])
+
+//all moust and key
+monitorEvents(window, ['mouse', 'key'])
+
+//common events
+monitorEvents(window, ['click', 'mouseover', 'textInput', 'scroll', 'resize'])
