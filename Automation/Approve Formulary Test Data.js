963354cd70e1d03872109f6bb0267c010e435c09
Curtis Tompkins <ctompkins@mmitnetwork.com>
2020-08-04 07:14:53 -0400

    up

--- Automation/Approve Formulary Test Data.js
+++ Automation/Approve Formulary Test Data.js
@@ -5,16 +5,30 @@ var todaysDate = mnth.toString() + "/" + d.getDate().toString() + "/" + d.getFul
 return [{
   "formName": "Formulary Test FDB " + todaysDate,
   'version': 1,
-  'tableID': 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00',
+  'tableID_1': 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00',
   'tableID_2': 'ctl00_cphMainContent_rgApprovalQueue_ctl00'
+  //reject radio button
+  'radioButton': 'ctl00_cphMainContent_rblApproveReject_1'
+},
+  {
+  "formName": "Formulary Test FDB " + todaysDate,
+  'version': 1,
+  'tableID_1': 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00',
+  'tableID_2': 'ctl00_cphMainContent_rgApprovalQueue_ctl00'
+  //approve radio button
+  'radioButton': 'ctl00_cphMainContent_rblApproveReject_0'
 },
   {
   "formName": "Formulary Test MS " + todaysDate,
   'version': 1,
-  'tableID': 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00',
+  'tableID_1': 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00',
   'tableID_2': 'ctl00_cphMainContent_rgApprovalQueue_ctl00'
+  //approve radio button
+  'radioButton': 'ctl00_cphMainContent_rblApproveReject_0'
 }];
 
+//set radio button to approve/reject
+document.getElementById(radioButton).checked = true;
 
 
 //clicks Approve/Rejct link of mactching formulary version
@@ -33,29 +47,36 @@ var clickApprove = function (tableID_2, formName, version) {
 }
 
 //for console test
-var d = new Date();
-var todaysDate = d.getMonth().toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString();
+//var d = new Date();
+//var mnth = d.getMonth()+1;
+//var todaysDate = mnth.toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString();
 
-clickApprove('ctl00_cphMainContent_rgApprovalQueue_ctl00', 'Formulary Test FDB '+ todaysDate, 1)
+//clickApprove('ctl00_cphMainContent_rgApprovalQueue_ctl00', 'Formulary Test FDB '+ todaysDate, 1)
 
 
 
 //clicks the row by formulary name alone
-var clickRow = function (tableID formName) {
-    var formTable = document.getElementById(tableID).getElementsByTagName('tbody')[0];
-    var formRows = formTable.rows
+var clickRow = function (tableID, formularyName) {
 
-            for (var r = 0; r < formRows.length; r++) {
-    					for  (var c = 0; c < formRows[r].cells.length; c++) {
-                if (formRows[r].cells[c].innerText === formName) {
-                  formRows[r].cells[0].click();
+var formTable = document.getElementById(tableID).getElementsByTagName('tbody')[0];
+var formRows = formTable.rows
 
-                }
-              }
-            }
-          };
+  for (var r = 0; r < formRows.length; r++) {
+      for  (var c = 0; c < formRows[r].cells.length; c++) {
+        if (formRows[r].cells[c].innerText === formularyName) {
+          formRows[r].cells[0].click()
+        }
+        //else throw new Error('Formulary not found.')
+      }
+    }
 
-tableID is 'ctl00_cphMainContent_flCurrent_rgFormularies_ctl00'
+  };
+//test data values for UI paramenters tableID and formularyName
+clickRow(tableID_1, formName)
+
+//Post for approval checkbox value
+document.getElementById('ctl00_cphMainContent_formularyEdit_cPostForApproval').checked
+
 
 //download vailidation for Full and Difference exports
 function readSheetNames(sheetName) {
