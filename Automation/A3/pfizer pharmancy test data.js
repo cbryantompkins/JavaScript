ff7e0560206b4e05c85a92ae098ca77ae0227905
ctompkins-mmit <65771709+ctompkins-mmit@users.noreply.github.com>
2020-08-14 14:37:57 -0400

    up

---
+++ Automation/A3/pfizer pharmancy test data.js
@@ -0,0 +1,16 @@
+return [{
+  "compareButton": "bv-Pharmacy"
+},{
+  "compareButton": "bvCompareAnotherDrug",
+  "filterButton": "btnAdvantage-Detail",
+  "filterList": "listAdvantage",
+  "filterTextID": "txtAdvantage-Detail",
+  "filterText" : "Parity Status",
+  "filterClearID" : 'bv-clear-filter-advantage',
+},{
+  "compareButton": "bvCompareOther"
+},{
+  "compareButton": "bvHistoricalChanges"
+},{
+  "compareButton": "btn-Switch_MultiProductChart1"
+}]
